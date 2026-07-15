4c20dda008a7469b1330ce283bed529ab1afeaca
Zammar <zzammar@college.harvard.edu>
2026-07-15 15:44:12 -0400

    FastAPI skeleton

---
+++ app/config.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+    database_url: str = "sqlite:///./dev.db"
+    huit_api_base_url: str
+    huit_api_key: str
